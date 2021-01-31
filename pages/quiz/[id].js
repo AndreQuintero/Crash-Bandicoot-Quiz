28e4bf9558ba0b354a4de284cfd93203274aceb4
andre.santos <andre.santos@spcbrasil.org.br>
2021-01-30 23:10:44 -0300

    pagina de quiz dinamica

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizComponent from '../../src/components/Quiz';
 
-export default function QuizDaGaleraPage({ dbExterno }) {
+export default function QuizDaGaleraPage({ bg, questions, theme }) {
   return (
-    <h1>teste</h1>
+    <ThemeProvider theme={theme}>
+      <QuizComponent external bg={bg} questions={questions} />
+    </ThemeProvider>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  const [projectName, githubUser] = id.split('___');
+  let dbExternal = '';
+
+  try {
+    const resp = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`);
+    dbExternal = await resp.json();
+  } catch (e) {
+    throw new Error('Falha ao obter dados');
+  }
+
+  return {
+    props: {
+      bg: dbExternal.bg,
+      questions: dbExternal.questions,
+      theme: dbExternal.theme,
+    },
+  };
 }
